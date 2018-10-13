4d395d36bdd746c3429fd8c4976881c99d43e64e
Bhoward19 <bhoward19@ymail.com>
2018-10-13 09:37:36 -0400

    attempt

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
     return <nav className="navbar navbar-light bg-danger">
         <a className="heading" href="/">
           <img src="images/header.png" width="50px" height="50px" alt="headerimg"/>
-          &nbsp;&nbsp;&nbsp; Clicky Game &nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp; Smash Click &nbsp;&nbsp;&nbsp;
           <img src="images/header.png" width="50px" height="50px" alt="headerimg" />
         </a>
       </nav>;
